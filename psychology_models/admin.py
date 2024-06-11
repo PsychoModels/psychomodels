45b314b56aea898493acaa64f3029845f74d696c
Vincent Verbrugh <vincent@verbrugh.nl>
2024-06-11 16:20:08 +0200

    refactor: model improvements and fixtures

--- psychology_models/admin.py
+++ psychology_models/admin.py
@@ -1,31 +1,15 @@
 from django.contrib import admin
 from markdownx.admin import MarkdownxModelAdmin
 
-from markdownx.widgets import AdminMarkdownxWidget
-
-# Register your models here.
 from . import models
 
-
-class PublicationAdmin(admin.ModelAdmin):
-    filter_horizontal = ["authors"]
-
-
-class PsychologyModelAdmin(admin.ModelAdmin):
-    list_display = ["title"]
-    filter_horizontal = ["framework"]
-
-    actions = []
-
-
 admin.site.register(models.Author)
-admin.site.register(models.PsychologyModel, PsychologyModelAdmin)
-admin.site.register(models.Proposal)
+admin.site.register(models.PsychologyModel)
 admin.site.register(models.Variable)
-admin.site.register(models.Modelvariable)
-admin.site.register(models.Language)
+# admin.site.register(models.Modelvariable)
+admin.site.register(models.ProgrammingLanguage)
 admin.site.register(models.Framework, MarkdownxModelAdmin)
-admin.site.register(models.Psychfield)
-admin.site.register(models.Publication, PublicationAdmin)
-admin.site.register(models.Softwarepackage)
-admin.site.register(models.Measurementinstrument)
+admin.site.register(models.PsychologyField)
+admin.site.register(models.Publication)
+admin.site.register(models.SoftwarePackage)
+admin.site.register(models.MeasurementInstrument)
--- psychology_models/models.py
+++ psychology_models/models.py
@@ -1,178 +1,148 @@
 from django.db import models
 from django.urls import reverse
-from django.utils.text import slugify
-
-from hashlib import sha256
+from autoslug import AutoSlugField
 
 from markdownx.models import MarkdownxField
 from markdownx.utils import markdownify
 
 
 class Author(models.Model):
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-    email = models.EmailField(max_length=254, null=True, blank=True)
-    institution = models.CharField(max_length=200, null=True, blank=True)
-    # phone = models.CharField(max_length=200)
-    # address = models.CharField(max_length=200)
-    # city = models.CharField(max_length=200)
-    # state = models.CharField(max_length=200)
-    # zip = models.CharField(max_length=200)
-    # country = models.CharField(max_length=200)
-    # role = models.CharField(max_length=200)
+    name = models.CharField()
 
-    user = models.OneToOneField(
-        "members.User", on_delete=models.SET_NULL, null=True, blank=True
-    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.first_name + " " + self.last_name
+        return self.name
 
 
 class Publication(models.Model):
-    unique_identifier = models.URLField(max_length=254, unique=True)
-    title = models.CharField(max_length=200)
+    url = models.URLField()
+    title = models.CharField()
     authors = models.ManyToManyField(Author)
 
-    year = models.IntegerField(null=True)
-    outlet = models.CharField(max_length=200, null=True, blank=True)
+    year = models.IntegerField(null=True, blank=True)
+    outlet = models.CharField(null=True, blank=True)
     volume = models.IntegerField(null=True, blank=True)
     issue = models.IntegerField(null=True, blank=True)
-    pages = models.CharField(max_length=200, null=True, blank=True)
+    pages = models.CharField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        auths = self.authors.all()
-        aut = f"{auths[0].last_name}, {auths[0].first_name[0]}"
-        if len(auths) > 1:
-            aut += f", et al"
-        out_str = f"{aut}"
-        if self.year is not None:
-            out_str += f". ({self.year})"
-        out_str += f". {self.title}"
-        out_str += f". {self.outlet}" if self.outlet is not None else ""
-        out_str += f", {self.volume}" if self.volume is not None else ""
-        out_str += f"({self.issue})" if self.issue is not None else ""
-        out_str += f": {self.pages}." if self.pages is not None else "."
-
-        return out_str
+        return self.title
 
 
-class Language(models.Model):
-    language_name = models.CharField(max_length=200)
-    language_documentation = models.CharField(max_length=200)
+class ProgrammingLanguage(models.Model):
+    name = models.CharField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.language_name
+        return self.name
 
 
 class Framework(models.Model):
-    framework_name = models.CharField(max_length=200)
-    framework_description = models.TextField(max_length=1500, null=True, blank=True)
+    name = models.CharField()
+    description = models.TextField(max_length=1500, null=True, blank=True)
     parent_framework = models.ForeignKey(
-        "self", on_delete=models.DO_NOTHING, null=True, blank=True
+        "self", on_delete=models.SET_NULL, null=True, blank=True
     )
     publication = models.ForeignKey(
-        Publication, on_delete=models.DO_NOTHING, null=True, blank=True
+        Publication, on_delete=models.SET_NULL, null=True, blank=True
     )
-
     explanation = MarkdownxField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     @property
     def formatted_explanation(self):
         return markdownify(self.explanation)
 
     def __str__(self):
-        return self.framework_name
+        return self.name
 
 
-class Softwarepackage(models.Model):
-    name = models.CharField(max_length=200)
+class SoftwarePackage(models.Model):
+    name = models.CharField()
     description = models.TextField(max_length=1500, null=True, blank=True)
-    documentation = models.URLField(max_length=254, null=True, blank=True)
-    language = models.ForeignKey("Language", on_delete=models.DO_NOTHING)
+    documentation = models.URLField(null=True, blank=True)
+    code_repository_url = models.URLField(null=True, blank=True)
+    programming_language = models.ForeignKey(
+        "ProgrammingLanguage", on_delete=models.PROTECT
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
 
-class Psychfield(models.Model):
+class PsychologyField(models.Model):
     discipline_name = models.CharField(max_length=200)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.discipline_name
 
 
 class Variable(models.Model):
-    variable_name = models.CharField(max_length=200)
-    variable_description = models.TextField(max_length=200)
+    name = models.CharField()
+    description = models.TextField(max_length=1500, null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.variable_name
+        return self.name
 
 
-class Measurementinstrument(models.Model):
-    instrument_name = models.CharField(max_length=200)
-    instrument_description = models.TextField(max_length=200, null=True)
-    instrument_publication = models.ForeignKey(Publication, on_delete=models.CASCADE)
-    additional_details = models.CharField(max_length=200, null=True, blank=True)
-    variables = models.ManyToManyField(Variable, blank=True)
-
-    def __str__(self):
-        return self.instrument_name
-
-
-class Psychmodel(models.Model):
+class PsychologyModel(models.Model):
     submitting_user = models.ForeignKey(
         "members.User",
-        on_delete=models.DO_NOTHING,
+        on_delete=models.SET_NULL,
         null=True,
         related_name="submitting_user",
     )
-    # co_authors = models.ManyToManyField("auth.User", related_name="co_authors")
+
     reviewer = models.ForeignKey(
         "members.User",
-        on_delete=models.DO_NOTHING,
+        on_delete=models.SET_NULL,
         null=True,
         related_name="reviewer",
         editable=False,
     )
 
-    reviewed = models.BooleanField(default=False)
-    # slug = models.SlugField(max_length=8, unique=True, editable=False, null=True)
+    publication = models.OneToOneField(Publication, on_delete=models.PROTECT)
 
-    publication = models.OneToOneField(Publication, on_delete=models.CASCADE)
-
-    model_name = models.CharField(max_length=200, unique=True)
+    title = models.CharField()
     description = models.TextField(max_length=3000)
-    explanation = MarkdownxField(null=True, blank=True)
-    # how_to_use_it = models.CharField(max_length=3000)
-    created_at = models.DateTimeField(auto_now_add=True)
-    language = models.ForeignKey("language", on_delete=models.DO_NOTHING)
+    explanation = MarkdownxField(null=True)
+
+    programming_language = models.ForeignKey(
+        "ProgrammingLanguage", on_delete=models.PROTECT
+    )
     framework = models.ManyToManyField(Framework)
-    softwarepackage = models.ManyToManyField(Softwarepackage, blank=True)
-    psychfield = models.ManyToManyField(Psychfield, blank=True)
+    software_package = models.ManyToManyField(SoftwarePackage, blank=True)
+    psychology_field = models.ManyToManyField(PsychologyField, blank=True)
 
-    codeURL = models.URLField(max_length=254, null=True, blank=True)
-    dataURL = models.URLField(max_length=254, null=True, blank=True)
+    code_repository_url = models.URLField(null=True, blank=True)
+    data_url = models.URLField(null=True, blank=True)
 
-    # model_file = models.FileField(upload_to="models/")
     model_variables = models.ManyToManyField(
         Variable, through="ModelVariable", blank=True
     )
 
-    def __str__(self):
-        return self.model_name
+    slug = AutoSlugField(populate_from="title")
 
-    # def create_slug(self, date=None):
-    #    if date is None:
-    #        date = self.created_at.strftime("%Y-%m-%d_%H:%M:%S")
-    #    slug = sha256(date.encode("utf-8"), usedforsecurity=False).hexdigest()[:10]
-    #    return slug
-
-    # def save(self, *args, **kwargs):
-    #    if not self.pk:
-    #        self.slug = slugify(self.create_slug())
-    #    super(PsychModel, self).save(*args, **kwargs)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self):
         return reverse("model_view", kwargs={"slug": self.slug})
@@ -181,39 +151,18 @@ class Psychmodel(models.Model):
     def formatted_explanation(self):
         return markdownify(self.explanation)
 
-
-class Proposal(models.Model):
-    title = models.CharField(max_length=200, default="New Model")
-    description = models.TextField(max_length=3000, null=True, blank=True)
-    publication = models.CharField(max_length=200, null=True, blank=True)
-
-    # def save_to_model(self, *args, **kwargs):
-    #     super(PsychModel, self).save(*args, **kwargs)
-
     def __str__(self):
         return self.title
 
 
-class Modelvariable(models.Model):
-    modelId = models.ForeignKey(
-        Psychmodel, on_delete=models.CASCADE, related_name="modelvariables"
+class ModelVariable(models.Model):
+    model_id = models.ForeignKey(
+        PsychologyModel, related_name="Variables", on_delete=models.RESTRICT
     )
-    variableId = models.ForeignKey(Variable, on_delete=models.CASCADE, null=True)
-    # variable_value = models.FloatField()
-    # variable_measurement_unit = models.CharField(max_length=200)
-    name = models.CharField(max_length=200, null=True)
-    measurementinstrument = models.ForeignKey(
-        Measurementinstrument, on_delete=models.DO_NOTHING, null=True, blank=True
-    )
+    variable_id = models.ForeignKey(Variable, null=True, on_delete=models.SET_NULL)
+
     details = models.TextField(max_length=1500, null=True, blank=True)
+    reference = models.CharField(max_length=12, null=True)
 
-    def __str__(self):
-        return self.name
-
-
-class Behaviour(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.CharField(max_length=1500)
-
-    def __str__(self):
-        return self.name
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
