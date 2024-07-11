21a0f26460d33f85b8bf9e39b36708999d338a61
Vincent Verbrugh <vincent@verbrugh.nl>
2024-07-11 23:03:07 +0200

    feat(backend): add fetch citation in admin

--- psychology_models/admin.py
+++ psychology_models/admin.py
@@ -5,8 +5,9 @@ from django_object_actions import (
     action,
     takes_instance_or_queryset,
 )
-
+from django.contrib import messages
 from . import models
+from .utils.get_doi_citations import get_doi_citations
 from .utils.set_publish_state import (
     publish_psychology_model,
     unpublish_psychology_model,
@@ -35,6 +36,13 @@ def action_unpublish(modeladmin, request, queryset):
     unpublish_entity(queryset)
 
 
+@admin.action(description="Fetch citation for publications")
+def action_get_doi_citations(modeladmin, request, queryset, refetch=False):
+    result_messages = get_doi_citations(queryset, refetch=refetch)
+    for result_message in result_messages:
+        messages.add_message(request, result_message[1], result_message[0])
+
+
 def published_state(self, obj):
     if obj.published_at is not None:
         return format_html(
@@ -47,30 +55,39 @@ def published_state(self, obj):
 
 
 class PsychologyModelAdmin(DjangoObjectActions, admin.ModelAdmin):
-    @action(label="Publish", description="Publish this model")  # optional
+    @action(label="Publish", description="Publish this model")
     @takes_instance_or_queryset
     def publish_this(self, request, obj):
         action_publish_psychology_model(self, request, obj)
 
-    @action(label="Unpublish", description="Unpublish this model")  # optional
+    @action(label="Unpublish", description="Unpublish this model")
     @takes_instance_or_queryset
     def unpublish_this(self, request, obj):
         action_unpublish_psychology_model(self, request, obj)
 
-    change_actions = ("publish_this", "unpublish_this")
+    @action(label="Fetch citation", description="(Re)-fetch citation for publications")
+    @takes_instance_or_queryset
+    def get_doi_citations(self, request, obj):
+        action_get_doi_citations(self, request, obj, True)
+
+    change_actions = ("publish_this", "unpublish_this", "get_doi_citations")
 
     readonly_fields = (
-        "publication_csl_json",
         "publication_citation",
         "published_at",
         "published_by",
     )
     exclude = (
+        "publication_csl_json",
         "publication_csl_fetched_at",
         "publication_citation_fetched_at",
     )
     list_display = ("title", "published_state")
-    actions = [action_publish_psychology_model, action_unpublish_psychology_model]
+    actions = [
+        action_publish_psychology_model,
+        action_unpublish_psychology_model,
+        action_get_doi_citations,
+    ]
 
     def published_state(self, obj):
         return published_state(self, obj)
@@ -97,9 +114,22 @@ class AdminMixin:
         return published_state(self, obj)
 
 
-class FrameworkAdmin(AdminMixin, admin.ModelAdmin):
-    readonly_fields = ("publication_csl_json", "publication_citation", "published_at")
+class FrameworkAdmin(DjangoObjectActions, admin.ModelAdmin):
+    @action(label="Fetch citation", description="(Re)-fetch citation for publications")
+    @takes_instance_or_queryset
+    def get_doi_citations(self, request, obj):
+        action_get_doi_citations(self, request, obj, True)
+
+    change_actions = ("get_doi_citations",)
+
+    actions = [action_publish, action_unpublish, action_get_doi_citations]
+
+    def published_state(self, obj):
+        return published_state(self, obj)
+
+    readonly_fields = ("publication_citation", "published_at")
     exclude = (
+        "publication_csl_json",
         "publication_csl_fetched_at",
         "publication_citation_fetched_at",
     )
---
+++ psychology_models/utils/get_doi_citations.py
@@ -0,0 +1,86 @@
+import requests
+from django.utils import timezone
+import re
+from doi_lookup.services import CrossRefClient
+from django.contrib import messages as django_messages
+
+
+def is_valid_doi(doi):
+    pattern = r"^10.\d{4,9}/[-._;()/:A-Z0-9]+$"
+    match = re.match(pattern, doi, re.IGNORECASE)
+    return bool(match)
+
+
+def fetch_and_save(
+    instance, client, fetch_function, attribute_name, fetched_at_name, refetch
+):
+    now = timezone.now()
+    messages = []
+
+    if getattr(instance, fetched_at_name) is not None or refetch:
+        try:
+            result = fetch_function(instance.publication_doi)
+            setattr(instance, attribute_name, result)
+            setattr(instance, fetched_at_name, now)
+            instance.save()
+            messages.append(
+                (
+                    f"fetched {attribute_name} for DOI {instance.publication_doi}  #({instance.id})",
+                    django_messages.SUCCESS,
+                )
+            )
+        except Exception as e:
+            if (
+                isinstance(e, requests.exceptions.HTTPError)
+                and e.response.status_code == 404
+            ):
+                messages.append(
+                    (
+                        f"{attribute_name} for DOI {instance.publication_doi} not found #({instance.id})",
+                        django_messages.ERROR,
+                    )
+                )
+            else:
+                messages.append(
+                    (
+                        f"error fetching {attribute_name} for DOI {instance.publication_doi}  #({instance.id})",
+                        django_messages.ERROR,
+                    )
+                )
+    return messages
+
+
+def get_doi_citations(queryset, refetch):
+    client = CrossRefClient()
+    messages = []
+
+    for instance in queryset:
+        if is_valid_doi(instance.publication_doi):
+            messages.extend(
+                fetch_and_save(
+                    instance,
+                    client,
+                    client.doi2apa,
+                    "publication_citation",
+                    "publication_citation_fetched_at",
+                    refetch,
+                )
+            )
+            messages.extend(
+                fetch_and_save(
+                    instance,
+                    client,
+                    client.doi2json,
+                    "publication_csl_json",
+                    "publication_csl_fetched_at",
+                    refetch,
+                )
+            )
+        else:
+            messages.append(
+                (
+                    f"Invalid DOI {instance.publication_doi} #({instance.id})",
+                    django_messages.ERROR,
+                )
+            )
+    return messages
