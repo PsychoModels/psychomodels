536d3630d91625a4071725477e90ac9ce2a84035
Vincent Verbrugh <vincent@verbrugh.nl>
2024-09-20 16:47:01 +0200

    fix: small bug fixes in template

--- psychomodels/settings.py
+++ psychomodels/settings.py
@@ -157,7 +157,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "members.User"
 
-EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
+EMAIL_BACKEND = os.getenv(
+    "EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend"
+)
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
