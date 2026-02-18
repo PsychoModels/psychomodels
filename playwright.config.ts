c4fe5ef2cfc62b983974dba8a748ba9b66eeb09f
Vincent Verbrugh <vincent@verbrugh.nl>
2026-02-18 21:52:29 +0100

    build: save playwright artifacts

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,6 +29,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
+
+    /* Record video for each test. See https://playwright.dev/docs/videos */
+    video: "retain-on-failure",
   },
 
   /* Configure projects for major browsers */
